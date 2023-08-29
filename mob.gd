de7c3805b9a543523ddee24e6a684a0b9333bd2a
Gavin Neal <grneal@bsu.edu>
2023-08-29 12:21:29 -0400

    Initial commit.

---
+++ mob.gd
@@ -0,0 +1,12 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var mob_types = $AnimatedSprite2D.sprite_frames.get_animation_names()
+	# randi() % n selects a random integer between 0 and n-1.
+	$AnimatedSprite2D.play(mob_types[randi() % mob_types.size()])
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
